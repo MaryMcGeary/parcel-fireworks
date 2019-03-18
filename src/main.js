af09be38dc0d1e3b544120a172243262fcb56a69
Mary McGeary <malicemcgeary@gmail.com>
2019-03-18 15:28:59 -0400

    MVP

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-export default () => {    // imports default stuff from ??
-    alert('it still works!');
-  };
+import './main.css'
+import Fireworks from 'fireworks-canvas';
+ 
+export default () => {
+  const container = document.getElementById('container');
+  const fireworks = new Fireworks(container);
+  fireworks.start();
+};
